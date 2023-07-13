d1112144f9ff60d3b570f2139fc383fa41ec3c58
Ezzinehajar <ezzine.hajar2003@gmail.com>
2023-07-13 11:03:32 +0200

    Sorts issuers names

--- src/main/java/com/pictet/technologies/ezzine/fileorchestrator/service/IssuerService.java
+++ src/main/java/com/pictet/technologies/ezzine/fileorchestrator/service/IssuerService.java
@@ -5,6 +5,9 @@ import com.pictet.technologies.ezzine.fileorchestrator.repository.IssuerReposito
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -22,6 +25,30 @@ public class IssuerService {
     public Set<String> getNamesOfIssuers() {
         return this.getAllIssuers().stream()
                 .map(IssuerEntity::getName)
-                .collect(Collectors.toSet());
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+    }
+
+    public List<IssuerEntity> filterNamesOfIssuer(String isin, String name, LocalDate date) {
+        if (name == null && isin == null && date == null) {
+            return this.getAllIssuers();
+        }
+
+        return this.getAllIssuers()
+                .stream()
+//                .filter(it -> filterByName(it, name))
+                .filter(issuerEntity -> {
+                    if (isin != null) return issuerEntity.getIsin().startsWith(isin);
+                    return true;
+                })
+                .filter(IssuerEntity -> {
+                    if (name != null) return IssuerEntity.getName().toUpperCase().contains(name.toUpperCase());
+                    return true;
+                })
+                .filter(issuerEntity -> {
+                    if (date != null) return issuerEntity.getDate().isBefore(date);
+                    return true;
+                })
+                .toList();
     }
 }
