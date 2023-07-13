f6970b24a3f090c4657ee381dc100d9a058dc512
Ezzinehajar <ezzine.hajar2003@gmail.com>
2023-07-13 09:07:51 +0200

    Add syntactic data

--- src/main/resources/db/changelog/syntactic-data/issuers.sql
+++ src/main/resources/db/changelog/syntactic-data/issuers.sql
@@ -5,7 +5,7 @@
 INSERT INTO Issuer (isin, name, date_added)
 VALUES ('AN8068571086', 'Schlumberger NV', '2021-01-01'),
        ('CA4457371090', 'Hunter Technology Corp', '2021-02-01'),
-       ('AT0000625504', 'OBERBANK AG-VORZ', '2021-01-05'),
+       ('AT0000625504', 'OBERBANK ', '2021-01-05'),
        ('ARBCOM4600U8', 'Sol Gel Technologies Ltd', '2021-04-01');
 
 
